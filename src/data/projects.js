62b786d50b383b057ba0288dd516b62376e6963b
dongmyoung kim <w0411191@nscc.ca>
2020-01-07 01:26:00 -0400

    my portfolio

---
+++ src/data/projects.js
@@ -0,0 +1,29 @@
+import project1 from '../assets/project1.png';
+import project2 from '../assets/project2.png';
+import project3 from '../assets/project3.png';
+
+const PROJECTS = [
+    {
+        id: 1,
+        title: 'Beach Resort - React Application',
+        description: 'A React App that I built, involving JS and core web dev concepts!',
+        link: 'https://github.com/dongmyoung6053/react-beach-resort-recording',
+        image: 'https://images.unsplash.com/photo-1569369926169-9ee7fb80adeb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1044&q=80'
+    },
+    {
+        id: 2,
+        title: 'Video Search - React Application',
+        description: 'A React App that I built, involving JS and core web dev concepts!',
+        link: 'https://github.com/dongmyoung6053/video-search',
+        image: 'https://images.unsplash.com/photo-1548328928-34db1c5fcc1f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'
+    },
+    {
+        id: 3,
+        title: 'Image Search - React Application',
+        description: 'A React App that I built, involving JS and core web dev concepts!',
+        link: 'https://github.com/dongmyoung6053/image-search',
+        image: 'https://images.unsplash.com/photo-1466378743612-9da217f53e55?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'
+    }
+];
+
+export default PROJECTS;
